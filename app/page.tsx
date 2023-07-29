5c7aa46818d693bc51b26b6ae30b8dd1f786a6e5
Alan Chou <alan10332000@gmail.com>
2023-07-30 02:32:39 +0800

    style: lint code

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,18 @@
+/* eslint-disable tailwindcss/no-custom-classname */
 "use client";
 
+import { useCompletion } from "ai/react";
+import clsx from "clsx";
 import { createElement, Fragment, useEffect, useRef, useState } from "react";
-import { unified, Plugin } from "unified";
-import { visit } from 'unist-util-visit'
-import remarkParse from "remark-parse";
-import remarkRehype from "remark-rehype";
 import rehypeReact from "rehype-react";
 import remarkGfm from "remark-gfm";
-import { useCompletion } from "ai/react";
-import { Input } from "@/components/ui/input";
+import remarkParse from "remark-parse";
+import remarkRehype from "remark-rehype";
+import { unified, Plugin } from "unified";
+import { visit } from "unist-util-visit";
+
 import { ModeToggle } from "@/components/theme-toggle";
-import clsx from "clsx";
+import { Input } from "@/components/ui/input";
 
 const rehypeChangeHrefToTargetBlank: Plugin = () => {
   return (tree: any) => {
@@ -20,7 +22,7 @@ const rehypeChangeHrefToTargetBlank: Plugin = () => {
       node.properties.rel = "noopener noreferrer";
     });
   };
-}
+};
 
 const useMarkdown = (markdown: string) => {
   const [Content, setContent] = useState(Fragment as any);
@@ -49,30 +51,15 @@ const randomPlaceLengthClasses = [
   "w-[100px] h-[20px]",
 ];
 
-const presetPrompts = [
-  "AI 的議程",
-  "Python 的議程",
-  "Rust 的議程",
-  "Go 的議程",
-  "區塊鏈相關",
-  "關於資安的議程",
-];
+const presetPrompts = ["AI 的議程", "Python 的議程", "Rust 的議程", "Go 的議程", "區塊鏈相關", "關於資安的議程"];
 
 const randomPresets = presetPrompts.sort(() => Math.random() - 0.5).slice(0, 3);
 
 export default function App() {
-  const {
-    completion,
-    input,
-    handleInputChange,
-    handleSubmit,
-    isLoading,
-    setInput,
-    setCompletion,
-    complete,
-  } = useCompletion({
-    api: "/api/search",
-  });
+  const { completion, input, handleInputChange, handleSubmit, isLoading, setInput, setCompletion, complete } =
+    useCompletion({
+      api: "/api/search",
+    });
 
   const formRef = useRef<HTMLFormElement>(null);
 
@@ -90,8 +77,8 @@ export default function App() {
   };
 
   return (
-    <div className="flex flex-col gap-6 justify-center items-center px-1 pb-2 h-full sm:px-0">
-      <span className="fixed top-1 right-1">
+    <div className="flex h-full flex-col items-center justify-center gap-6 px-1 pb-2 sm:px-0">
+      <span className="fixed right-1 top-1">
         <ModeToggle />
       </span>
 
@@ -103,20 +90,18 @@ export default function App() {
           "pt-5 justify-around gap-6": completion,
         })}
       >
-        <h1 className="text-4xl font-bold">
-          COSCUP 2023 x OpenAI 議程搜尋系統
-        </h1>
+        <h1 className="text-4xl font-bold">COSCUP 2023 x OpenAI 議程搜尋系統</h1>
 
-        <label className="flex flex-col gap-1 items-center">
+        <label className="flex flex-col items-center gap-1">
           <div className="relative">
             <Input
-              className="py-6 px-4 max-w-full text-2xl w-[300px]"
+              className="w-[300px] max-w-full px-4 py-6 text-2xl"
               placeholder="請輸入關鍵字"
               value={input}
               onChange={handleInputChange}
             />
 
-            <small className="text-muted-foreground absolute -right-2 translate-x-[100%] top-[18px]">
+            <small className="absolute -right-2 top-[18px] translate-x-[100%] text-muted-foreground">
               {(input || completion) && (
                 <a className="text-sm underline" onClick={clear} href="#">
                   清除搜尋結果
@@ -131,13 +116,9 @@ export default function App() {
               <>
                 <br />
                 或是，試試以下幾種：
-                <ul className="list-disc list-inside">
+                <ul className="list-inside list-disc">
                   {randomPresets.map((prompt) => (
-                    <li
-                      className="underline cursor-pointer"
-                      key={prompt}
-                      onClick={() => onPresetClick(prompt)}
-                    >
+                    <li className="cursor-pointer underline" key={prompt} onClick={() => onPresetClick(prompt)}>
                       {prompt}
                     </li>
                   ))}
@@ -150,61 +131,40 @@ export default function App() {
 
       {/* placeholder */}
       {isLoading && !completion && (
-        <div className="flex flex-col gap-2 justify-center mx-auto w-full animate-pulse max-w-[500px]">
+        <div className="mx-auto flex w-full max-w-[500px] animate-pulse flex-col justify-center gap-2">
           {/* randomize text placeholder */}
-          {new Array(Math.floor(Math.random() * 3) + 5)
-            .fill(0)
-            .map((i, index) => {
-              const randomLengthClass =
-                randomPlaceLengthClasses[
-                Math.floor(Math.random() * randomPlaceLengthClasses.length)
-                ];
+          {new Array(Math.floor(Math.random() * 3) + 5).fill(0).map((i, index) => {
+            const randomLengthClass =
+              randomPlaceLengthClasses[Math.floor(Math.random() * randomPlaceLengthClasses.length)];
 
-              return (
-                <div
-                  className={`bg-gray-300 rounded-lg ${randomLengthClass}`}
-                  key={`placeholder-${index}`}
-                />
-              );
-            })}
+            return <div className={`rounded-lg bg-gray-300 ${randomLengthClass}`} key={`placeholder-${index}`} />;
+          })}
         </div>
       )}
 
       {completion && (
-        <div className="overflow-auto px-4 w-full max-w-[800px] markdown-body max-h-[500px]">
+        <div className="markdown-body max-h-[500px] w-full max-w-[800px] overflow-auto px-4">
           <Content />
         </div>
       )}
 
       {/* floating footer to the bottom */}
-      <div className="flex fixed bottom-1 flex-col gap-2 justify-center items-center w-full">
-        <div className="flex gap-2 justify-center items-center">
+      <div className="fixed bottom-1 flex w-full flex-col items-center justify-center gap-2">
+        <div className="flex items-center justify-center gap-2">
           <span className="text-sm text-muted-foreground">
-            <a
-              href="https://coscup.org/2023/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
+            <a href="https://coscup.org/2023/" target="_blank" rel="noopener noreferrer">
               COSCUP 2023
             </a>
           </span>
 
           <span className="text-sm text-muted-foreground">
-            <a
-              href="https://github.com/Yukaii/coscup-session-openai"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
+            <a href="https://github.com/Yukaii/coscup-session-openai" target="_blank" rel="noopener noreferrer">
               GitHub
             </a>
           </span>
 
           <span className="text-sm text-muted-foreground">
-            <a
-              href="https://yukai.dev"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
+            <a href="https://yukai.dev" target="_blank" rel="noopener noreferrer">
               {`Hi, I'm Yukai`}
             </a>
           </span>
