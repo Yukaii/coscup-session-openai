9c4c24e5fe3454ebead26769f72ee53705bdbbe7
Alan Chou <alan10332000@gmail.com>
2023-07-30 02:34:13 +0800

    chore: use correct html lang

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="zh-Hant" suppressHydrationWarning>
       <body>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           {children}
