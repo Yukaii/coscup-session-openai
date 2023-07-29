611d1337dfa3e314c2b5f65f5758bb10a85de815
Yukai Huang <yukaihuangtw@gmail.com>
2023-07-29 21:15:12 +0800

    feat: nice input

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 
 import { useCompletion } from 'ai/react'
 import { Input } from "@/components/ui/input"
+import { ModeToggle } from '@/components/theme-toggle'
 
 export default function App() {
   const { completion, input, handleInputChange, handleSubmit, isLoading, complete } = useCompletion({
@@ -9,25 +10,27 @@ export default function App() {
   })
 
   return (
-    <div>
-      <pre>
-        <code>
-          {/* isLoading: {JSON.stringify(isLoading)} */}
-          {/* complete: {JSON.stringify(complete)} */}
-          {completion}
-        </code>
-      </pre>
-
-
+    <div className='flex justify-center items-center h-full'>
+      <span className='fixed right-1 top-1'>
+        <ModeToggle />
+      </span>
       <form onSubmit={handleSubmit}>
         <label>
-          Say something...
           <Input
+            className='py-6 px-4 text-2xl w-[300px] max-w-full'
+            placeholder='請輸入關鍵字'
             value={input}
             onChange={handleInputChange}
           />
+          <small className='text-muted-foreground'>來問問關於今年 COSCUP 議程的一些事吧！</small>
         </label>
       </form>
+
+      {/* <pre> */}
+      {/*   <code> */}
+      {/*     {completion} */}
+      {/*   </code> */}
+      {/* </pre> */}
     </div>
   )
 }
